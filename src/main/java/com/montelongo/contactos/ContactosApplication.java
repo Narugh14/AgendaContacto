3bf1fca6337f1768d0c6f784f26636bd0c3fab87
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2024-12-03 10:08:17 -0600

    Spring Initializr

---
+++ src/main/java/com/montelongo/contactos/ContactosApplication.java
@@ -0,0 +1,13 @@
+package com.montelongo.contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactosApplication.class, args);
+	}
+
+}
